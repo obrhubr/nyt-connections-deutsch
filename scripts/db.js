758c5003c8192b9dd7cb4b4589f8847c97ce7610
obrhubr <obrhubr@gmail.com>
2024-06-02 22:52:21 +0200

    make userId persistent

--- scripts/db.js
+++ scripts/db.js
@@ -4,7 +4,14 @@ if (hosted) {
 	// Initialize Firestore through Firebase
 	const app = firebase.initializeApp(keys);
 	var analytics = firebase.analytics(app);
-	var userId = crypto.randomUUID();
+
+	var userId = "";
+	if (localStorage.getItem("userId") == null) {
+		userId = crypto.randomUUID();
+		userId = localStorage.setItem("userId")
+	} else {
+		userId = localStorage.getItem("userId")
+	};
 	analytics.setUserId(userId);
 	
 	var db = firebase.firestore();
